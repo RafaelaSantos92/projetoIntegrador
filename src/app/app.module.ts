2e2e883a6301a05b645ead4abd1da529edb5580d
Rafaela Santos <--santos.rafaela2991@gmail.com>
2022-05-31 11:36:45 -0300

    Implementação do componente cadastro-login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { CadastroLoginComponent } from './cadastro-login/cadastro-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    CadastroLoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cadastro-login/cadastro-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-login',
+  templateUrl: './cadastro-login.component.html',
+  styleUrls: ['./cadastro-login.component.css']
+})
+export class CadastroLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
